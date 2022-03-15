38d6b1804d08e64ce645e8e3b2efd4bbdb60d12e
ngohoanglong <ngohoanglongptit@gmail.com>
2022-03-15 11:24:56 +0700

    update

--- pages/api/graphql.js
+++
@@ -1,7 +0,0 @@
-import fetchGraphqlApi from "../../libs/api/fetch-graphql-api";
-
-export default async function handler(req, res) {
-  const { query, variables } = req.body;
-  const result = await fetchGraphqlApi(query, { variables });
-  res.status(200).json(result);
-}
